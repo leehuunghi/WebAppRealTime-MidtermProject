87b320623da77e1306729cc5eb96e85e4e577bdb
DESKTOP-4H2CDN2\Admin <trannha132@gmail.com>
2018-11-05 18:15:50 +0700

    App1: Frontend init vue project, basic page

---
+++ App1/Frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+   <router-view :to="{ name: 'FormInformation' }"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ App1/Frontend/src/components/FormInformation.vue
@@ -0,0 +1,74 @@
+<template>
+  <form action="" method="POST" v-on:submit.prevent="sendinfo">
+
+            <div class="row">
+                <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+                    <div class="form-group">
+                        <label>HỌ TÊN</label>
+                        <input type="text" name="hoten" class="form-control" v-model="formdata.name" id="hoten"
+                            placeholder="Trần Văn A">
+                    </div>
+                    <div class="form-group">
+                        <label>SỐ ĐIỆN THOẠI</label>
+                        <input type="text" name="sdt" class="form-control" v-model="formdata.phone" id="sdt" placeholder="01234567xx">
+                    </div>
+                </div>
+                <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+                    <div class="form-group">
+                        <label>ĐỊA CHỈ ĐÓN</label>
+                        <input type="text" name="diachi" class="form-control" v-model="formdata.adress" id="diachi"
+                            placeholder="227 Nguyễn Văn Cừ, P4, Q5">
+                    </div>
+                    <div class="form-group">
+                        <label>GHI CHÚ</label>
+                        <input type="text" name="ghichu" class="form-control" v-model="formdata.note" id="ghichu"
+                            placeholder="Đón ở cổng...">
+                    </div>
+                </div>
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                    <button type="submit" id="btnSubmit">
+                        <img src="/static/image/right-arrow.png" id="imgSubmit">
+                    </button>
+                </div>
+            </div>
+
+        </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      title: "Send Information",
+      formdata: {},
+      message: "",
+      success: 0
+    };
+  },
+  methods: {
+    sendinfo() {
+      axios
+        .post("http://172.16.1.142:3000/api/bookingBike/book", this.formdata)
+        .then(response => {
+          console.log(response);
+          alert(response.data.success);
+          if (response.data.success > 0) {
+            alert("true");
+            this.message = "You send information success";
+            this.success = response.data.success;
+          } else {
+            this.message = "You send information fail";
+            this.success = response.data.success;
+          }
+          alert(this.message);
+        })
+        .catch(err => {
+          alert(err);
+        });
+    }
+  }
+};
+</script>
+
