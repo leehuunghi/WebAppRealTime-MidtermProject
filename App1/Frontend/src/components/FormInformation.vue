51e9322a8c1dab9e3223cf08bfeb477940257ca3
donguyen131197 <37667792+donguyen131197@users.noreply.github.com>
2018-11-22 21:57:49 +0700

    Chỉnh sửa app1

--- App1/Frontend/src/components/FormInformation.vue
+++ App1/Frontend/src/components/FormInformation.vue
@@ -1,5 +1,40 @@
 <template>
 <div>
+    <div id="modalFail" class="modal-backdrop" style="height: 100% !important; display: none; width: 100% !important; background-color: transparent !important;">
+        <div class="modal" role="dialog" style="display: block; padding-top: 200px; background-color: rgba(0,0,0,0.5);">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content" style="padding: 20px; border: none !important; border-radius: 10px;">
+                    <div>
+                        <h5 class="mdTitle">Lỗi gửi</h5>
+                    </div>
+                    <div class="mdDes">
+                        <p>Gửi thất bại.</p>
+                    </div>
+                    <div style="text-align: right">
+                        <button id="tryAgain" type="button" class="mdBtn">Nhập lại</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div id="modalSucceed" class="modal-backdrop" style="height: 100% !important; display: none; width: 100% !important; background-color: transparent !important;">
+        <div class="modal" role="dialog" style="display: block; padding-top: 200px; background-color: rgba(0,0,0,0.5);">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content" style="padding: 20px; border: none !important; border-radius: 10px;">
+                    <div>
+                        <h5 class="mdTitle">Thành công</h5>
+                    </div>
+                    <div class="mdDes">
+                        <p>Gửi thành công.</p>
+                    </div>
+                    <div style="text-align: right">
+                        <button id="close" type="button" class="mdBtn">Đóng</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
+
    <div class="coverBg">
         <div id="logoutBtn" style="position: fixed; width: 100%; text-align: right; z-index: 99;">
             <button class="logoutBtn">ĐĂNG XUẤT<Table></Table></button>
@@ -12,22 +47,22 @@
             <form style="margin: 20px 50px 0 150px;" action="" method="POST" v-on:submit.prevent="sendinfo">
                 <div class="row">
                     <div class="col-md-5">
-                        <p for="username" class="inputTit" style="margin-left: -230px;">HỌ TÊN</p>
-                        <input id="username" type="text" style="width: 330px;" name="name" class="form-control" v-model="formdata.name"
+                        <p for="name" class="inputTit" style="margin-left: -230px;">HỌ TÊN</p>
+                        <input id="name" type="text" style="width: 330px;" name="name" v-model="formdata.name"
                             placeholder="Trần Văn A">
-                        <p for="username" class="inputTit" style="margin-left: -190px; margin-top: 20px">ĐỊA CHỈ ĐÓN</p>
-                        <input id="username" type="text" name="adress" class="form-control" v-model="formdata.address"
+                        <p for="address" class="inputTit" style="margin-left: -190px; margin-top: 20px">ĐỊA CHỈ ĐÓN</p>
+                        <input id="address" type="text" name="adress" v-model="formdata.address"
                             placeholder="227 Nguyễn Văn Cừ, P4, Q5" style="width: 330px;"> 
                     </div>
                     <div class="col-md-5">
-                        <p for="username" class="inputTit" style="margin-left: -170px;">SỐ ĐIỆN THOẠI</p>
-                        <input id="username" type="text" name="phone" class="form-control" v-model="formdata.phone" placeholder="01234567xx" style="width: 330px;">
-                        <p for="username" class="inputTit" style="margin-left: -225px; margin-top: 20px">GHI CHÚ</p>
-                        <input id="username" type="text" name="note" class="form-control" v-model="formdata.note"
+                        <p for="phone" class="inputTit" style="margin-left: -170px;">SỐ ĐIỆN THOẠI</p>
+                        <input id="phone" type="text" name="phone" v-model="formdata.phone" placeholder="01234567xx" style="width: 330px;">
+                        <p for="note" class="inputTit" style="margin-left: -225px; margin-top: 20px">GHI CHÚ</p>
+                        <input id="note" type="text" name="note" v-model="formdata.note"
                             placeholder="Đón ở cổng..." style="width: 330px;">
                     </div>
                     <div class="col-md-1">
-                        <button class="submitBtn" style="margin-top: 122px; margin-left: -20px"><img src="/static/icons/next.png"></button>
+                        <button type="submit" class="submitBtn" style="margin-top: 122px; margin-left: -20px"><img src="/static/icons/next.png"></button>
                     </div>
                 </div>
             </form>
@@ -37,6 +72,16 @@
 </template>
 
 <script>
+
+$(document).ready(function() {
+  $("#tryAgain").click(function() {
+    $("#modalFail").fadeOut("fast");
+  });
+  $("#close").click(function() {
+    $("#modalSucceed").fadeOut("fast");
+  });
+});
+
 import axios from "axios";
 
 export default {
@@ -51,20 +96,20 @@ export default {
   methods: {
     sendinfo() {
       axios
-        .post("http://192.168.1.10:3000/api/bookingBike/book", this.formdata, {
-            headers: {
-                'x-access-token':  this.$session.get('access_token')
-            }
+        .post("http://172.16.9.218:3000/api/bookingBike/book", this.formdata, {
+          headers: {
+            'x-access-token': this.$localStorage.get('access_token')
+          }
         })
         .then(response => {
           if (response.data.success > 0) {
-              alert("Send info success");
+            $("#modalSucceed").fadeIn();
           } else {
-            alert("Send info fail");
+            $("#modalFail").fadeIn();
           }
         })
         .catch(err => {
-          alert(err);
+          $("#modalFail").fadeIn();
         });
     }
   }
